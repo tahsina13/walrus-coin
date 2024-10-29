01d58399247ecae79dd230f7fd62d487ebf01ba4
Joseph Hanrahan <joseph.hanrahan@stonybrook.edu>
2024-10-29 10:19:34 -0400

    btcwallet

--- frontend/src/renderer/src/components/RegisterPage.tsx
+++ frontend/src/renderer/src/components/RegisterPage.tsx
@@ -8,22 +8,20 @@ function RegisterPage(): JSX.Element {
   const [walletPassword, setWalletPassword] = useState('');
   const [walletExists, setWalletExists] = useState(false);
   const [accountName, setAccountName] = useState('');
-  const [error_message, set_error_message] = useState('');
 
   const navigate = useNavigate();
 
   const handleCreateWallet = async () => {
-    if(walletPassword) {
-      await window.versions.createWallet("../backend/btcwallet/btcwallet.exe", ["--create"], [walletPassword, walletPassword, "n", "n", "OK"]);
-      localStorage.setItem("walletExists", "true");
-      navigate('/firstLogin');
-      // await window.versions.startProcess("../backed/btcd/cmd/btcctl", ["--wallet", "--rpcuser=user", "--rpcpass=password", "--rpcserver=localhost:8332", "listaccounts"]);
-    }
-    else {
-      set_error_message("The password cannot be blank.");
+    // await window.versions
+    await window.versions.createWallet("../backend/btcwallet/btcwallet", ["--create"], [walletPassword, walletPassword, "n", "n", "OK"]);
+    returnHome();
+    // await window.versions.startProcess("../backed/btcd/cmd/btcctl", ["--wallet", "--rpcuser=user", "--rpcpass=password", "--rpcserver=localhost:8332", "listaccounts"]);
+  }
+  const handleCreateAccount = async () => {
+    if (true){ // add conditions when we figure out login
+      navigate('/status');
     }
   }
-
   const returnHome = async () => {
     navigate('/sign-in')
   }
@@ -32,7 +30,6 @@ function RegisterPage(): JSX.Element {
   useEffect(() => {
     const checkIfWalletExists = async () => {
       try {
-
         let log =  await window.versions.startProcess("../backend/btcwallet/btcwallet", []);
         console.log(log)
         setWalletExists(true)
@@ -47,45 +44,40 @@ function RegisterPage(): JSX.Element {
 
   if(!walletExists){
     return (
-        <div style={{ backgroundColor: '#997777' }} className="container flex justify-center items-center h-screen w-screen">
+        <div className="container flex justify-center items-center h-screen w-screen">
             <div className="flex flex-col items-center">
-              <div className="header">
-                  <div className="text-xl text-white"> Create a Wallet </div>
-              </div>
-              <div className="inputs mt-4">
-                <div className="input mb-4 flex space-x-2">
-                    <input 
-                        type="password" 
-                        placeholder='Password' 
-                        className="border border-gray-300 p-2 rounded focus:outline-none"
-                        value={walletPassword}
-                        onChange={(event)=>{setWalletPassword(event.target.value)}}
-                        required
-                        />
-                        <div className="register-container flex justify-end w-full">
-                    <div className="submit-container flex">
-                      <button 
-                          className="submit bg-yellow-900 text-white px-4 py-2 rounded hover:bg-black duration-300 disabled:bg-gray-300 disabled:text-gray-500 cursor-pointer disabled:cursor-not-allowed"
-                          type='button'
-                          onClick={handleCreateWallet}
-                          // disabled={!inputValue}
-                          >
-                            Create
-                      </button>
-                    </div>
-                  </div>
+                <div className="header">
+                    <div className="text"> Create a Wallet </div>
                 </div>
-              </div>
-              <div className='text-white'>{error_message}</div>
-              <div className='flex space-x-1'>
-                <div>Not sure yet?</div><div onClick={()=>{navigate('/sign-in')}} className='cursor-pointer text-blue-800 underline'>Return to home</div>
-              </div>
+                <div className="inputs mt-4">
+                    <div className="input mb-4">
+                        <input 
+                            type="password" 
+                            placeholder='Password' 
+                            className="border border-gray-300 p-2 rounded focus:outline-none"
+                            value={walletPassword}
+                            onChange={(event)=>{setWalletPassword(event.target.value)}}
+                            />
+                    </div>
+                    <div className="submit-container flex">
+                      <div className="register-container flex justify-end w-full">
+                          <button 
+                            className="submit bg-blue-500 text-white p-2 rounded disabled:bg-gray-300 disabled:text-gray-500 cursor-pointer disabled:cursor-not-allowed"
+                            type='button'
+                            onClick={handleCreateWallet}
+                            // disabled={!inputValue}
+                            >
+                              Create
+                          </button>
+                      </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
   } else{
     return (
-        <div style={{ backgroundColor: '#997777' }} className="container flex justify-center items-center h-screen w-screen">
+        <div className="container flex justify-center items-center h-screen w-screen">
             <div className="flex flex-col items-center">
                 <div className="header">
                     <div className="text"> You already have a wallet! </div>
@@ -94,7 +86,7 @@ function RegisterPage(): JSX.Element {
                     <div className="submit-container flex">
                       <div className="register-container flex justify-end w-full">
                           <button 
-                            className="submit bg-yellow-900 text-white px-4 py-2 rounded hover:bg-black duration-300 disabled:bg-gray-300 disabled:text-gray-500 cursor-pointer disabled:cursor-not-allowed"
+                            className="submit bg-blue-500 text-white p-2 rounded disabled:bg-gray-300 disabled:text-gray-500 cursor-pointer disabled:cursor-not-allowed"
                             type='button'
                             onClick={returnHome}
                             >
